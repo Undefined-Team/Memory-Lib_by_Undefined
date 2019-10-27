5f083959857a9d801b582d5395dfeb5b0771bd84
sbedene <sylvainbedene@gmail.com>
2019-10-27 15:39:03 +0100

    fix segfault mem cmp

--- res/src/cmp.c
+++ res/src/cmp.c
@@ -2,6 +2,8 @@
 
 int        ud_mem_cmp(void *dst, void *src, size_t len)
 {
+    if (!dst) return -1;
+    if (!src) return 1;
     char *dstc = (char*)dst;
     char *srcc = (char*)src;
 	while (len-- > 0) if (*dstc++ != *srcc++) return *--dstc - *--srcc;
